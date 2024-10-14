9f5e8a2ae5aabaa747b9b3fd317c87d978e78251
dldbcosmos <61865148+dldbcosmos@users.noreply.github.com>
2024-10-14 16:46:59 +0900

    Update setup.py
    text correction

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='test'
-    version='0.0.1'
-    description='python get string'
-    url= 'https://github.com/dldbcosmos/git_test.git'
-    author='eu'
-    author email='dldbcosmos.gmail.com'
-    license='free'
-    packages=find_packages()
+    name='test',
+    version='0.0.1',
+    description='python get string',
+    url= 'https://github.com/dldbcosmos/git_test.git',
+    author='eu',
+    author email='dldbcosmos.gmail.com',
+    license='free',
+    packages=find_packages(),
     install_requires=['pandas'],
 )
     
